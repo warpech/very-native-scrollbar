6792b1a80eca021395358b3dfcd8d4fafa03d955
Marcin Warpechowski <warpech@gmail.com>
2013-12-16 16:24:59 +0100

    Very Native Scrollbar v0.0.20131216

--- src/very-native-scrollbar.js
+++ src/very-native-scrollbar.js
@@ -1,5 +1,5 @@
 ﻿/**
- * Very Native Scrollbar
+ * Very Native Scrollbar v0.0.20131216
  * This small library gives you a tiny API to create and interact with a native browser scrollbar.
  *
  * Copyright 2013, Marcin Warpechowski
